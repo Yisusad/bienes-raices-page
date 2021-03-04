f923ebd42135972f526ddbadcf8fb5f57ce9aaf7
Yisusad <jesuscheoz@gmail.com>
2021-03-03 23:29:02 -0300

    Preparando CRUD php

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bienes Raíces</title>
-    <link rel="stylesheet" href="build/css/app.css">
+    <link rel="stylesheet" href="/build/css/app.css">
 </head>
 <body>
     
@@ -13,15 +13,15 @@
         <div class="contenedor contenido-header">
             <div class="barra">
                 <a href="/bienesraices-page/index.php">
-                    <img src="build/img/logo.svg" alt="imagen logo">
+                    <img src="/build/img/logo.svg" alt="imagen logo">
                 </a>
 
                 <div class="mobile-menu">
-                    <img src="build/img/barras.svg" alt="icono hamburguesa">
+                    <img src="/build/img/barras.svg" alt="icono hamburguesa">
                 </div>
 
                 <div class="derecha">
-                    <img class="dark-mode-boton" src="build/img/dark-mode.svg" alt="boton modo oscuro">
+                    <img class="dark-mode-boton" src="/build/img/dark-mode.svg" alt="boton modo oscuro">
                     <nav class="navegacion">
                         <a href="nosotros.php">Nosotros</a>
                         <a href="anuncios.php">Anuncios</a>
