cc13df2642601f49639730a45e8d41990f9043ac
Yisusad <jesuscheoz@gmail.com>
2021-03-09 22:40:46 -0300

    Conectando a DB

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -1,4 +1,37 @@
 <?php 
+
+    //Base de datos
+
+    require '../../includes/config/database.php';
+
+    $db = conectarDB();
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+
+        $titulo = $_POST['titulo'];
+        $precio = $_POST['precio'];
+        $descripcion = $_POST['descripcion'];
+        $habitaciones = $_POST['habitaciones'];
+        $wc = $_POST['wc'];
+        $estacionamiento = $_POST['estacionamiento'];
+        $vendedorId = $_POST['vendedor'];
+
+        //Insertar a BD
+
+        $query = "INSERT INTO propiedades (titulo, precio, descripcion, habitaciones, wc, estacionamiento, vendedorId) VALUES ( '$titulo',
+        '$precio', '$descripcion', '$habitaciones', '$wc', '$estacionamiento', '$vendedorId' )";
+
+        //echo $query;
+        $resultado = mysqli_query($db, $query);
+
+        if($resultado){
+            echo "Insertado correctamente";
+        }
+    }
+
     require '../../includes/funciones.php';
     
     incluirTemplate('header');
@@ -8,6 +41,48 @@
         <h1>Crear</h1>
 
         <a href="/admin" class="boton boton-verde">Volver</a>
+
+        <form action="/admin/propiedades/crear.php" class="formulario" method="POST">
+            <fieldset>
+                <legend>Información General</legend>
+
+                <label for="titulo">Titulo</label>
+                <input type="text" id="titulo" name="titulo" placeholder="Título de propiedad">
+
+                <label for="precio">Precio</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio de propiedad">
+
+                <label for="imagen">Imagen</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <label for="descripcion">Descripción</label>
+                <textarea id="descripcion" name="descripcion"></textarea>
+            </fieldset>
+
+            <fieldset>
+                <legend>Información de Propiedad</legend>
+
+                <label for="habitaciones">Habitaciones</label>
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="wc">Baños</label>
+                <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
+
+                <label for="estacionamiento">Estacionamiento</label>
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9">               
+            </fieldset>
+
+            <fieldset>
+                <legend>Vendedor</legend>
+
+                <select name="vendedor">
+                    <option value="1">Juan</option>
+                    <option value="2">Karen</option>
+                </select>
+            </fieldset>
+
+            <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+        </form>
     </main>
 
     <?php incluirTemplate('footer'); ?>
--- includes/config/database.php
+++ includes/config/database.php
@@ -1,4 +1,12 @@
 <?php
 
+function conectarDB() : mysqli {
+    $db = mysqli_connect('localhost', 'root', '', 'bienes_raices');
 
-?>
+    if(!$db){
+        echo "Error de conexión";
+        exit;
+    } 
+
+    return $db;
+}
