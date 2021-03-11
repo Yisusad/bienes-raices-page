c351e8c2de919e3fe6df78389281ffb2d5d757de
Yisusad <jesuscheoz@gmail.com>
2021-03-11 16:56:44 -0300

    Mantener valores escritos despúes de mensajes de error-formulario

--- admin/propiedades/crear.php
+++ admin/propiedades/crear.php
@@ -9,6 +9,15 @@
     //Arreglo para mensajes de errores
     $errores = [];
 
+    $titulo = '';
+    $precio = '';
+    $descripcion = '';
+    $habitaciones = '';
+    $wc = '';
+    $estacionamiento = '';
+    $vendedorId = '';
+    
+
     if($_SERVER['REQUEST_METHOD'] === 'POST'){
         //echo "<pre>";
         //var_dump($_POST);
@@ -83,29 +92,29 @@
                 <legend>Información General</legend>
 
                 <label for="titulo">Titulo</label>
-                <input type="text" id="titulo" name="titulo" placeholder="Título de propiedad">
+                <input type="text" id="titulo" name="titulo" placeholder="Título de propiedad" value="<?php echo $titulo; ?>">
 
                 <label for="precio">Precio</label>
-                <input type="number" id="precio" name="precio" placeholder="Precio de propiedad">
+                <input type="number" id="precio" name="precio" placeholder="Precio de propiedad" value="<?php echo $precio; ?>">
 
                 <label for="imagen">Imagen</label>
                 <input type="file" id="imagen" accept="image/jpeg, image/png">
 
                 <label for="descripcion">Descripción</label>
-                <textarea id="descripcion" name="descripcion"></textarea>
+                <textarea id="descripcion" name="descripcion"><?php echo $descripcion; ?></textarea>
             </fieldset>
 
             <fieldset>
                 <legend>Información de Propiedad</legend>
 
                 <label for="habitaciones">Habitaciones</label>
-                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej: 3" min="1" max="9" value="<?php echo $habitaciones; ?>">
 
                 <label for="wc">Baños</label>
-                <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9">
+                <input type="number" id="wc" name="wc" placeholder="Ej: 3" min="1" max="9" value="<?php echo $wc; ?>">
 
                 <label for="estacionamiento">Estacionamiento</label>
-                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9">               
+                <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej: 3" min="1" max="9" value="<?php echo $estacionamiento; ?>">               
             </fieldset>
 
             <fieldset>
